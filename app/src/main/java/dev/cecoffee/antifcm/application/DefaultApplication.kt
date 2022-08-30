d95fdddb4337e6df3edcb6e0a4baa2d83a7e3ee4
moxiout@gmail.com <moxiout@gmail.com>
2022-08-30 17:56:01 +0800

    try fix activate crash

--- app/src/main/java/dev/cecoffee/antifcm/application/DefaultApplication.kt
+++ app/src/main/java/dev/cecoffee/antifcm/application/DefaultApplication.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.highcapable.yukihookapi.hook.xposed.application.ModuleApplication
 
 class DefaultApplication : ModuleApplication() {
+    init {
+    }
+
     companion object {
         lateinit var shared: SharedPreferences
         fun isCancelHeartbeat(): Boolean {
@@ -28,13 +31,13 @@ class DefaultApplication : ModuleApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        shared = this.applicationContext.getSharedPreferences("data", MODE_PRIVATE)
         /**
          * 跟随系统夜间模式
          * Follow system night mode
          */
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         // Your code here.
-        shared = this.applicationContext.getSharedPreferences("data", MODE_PRIVATE)
         val editor = shared.edit()
         if (!shared.contains("cancelHeartbeat")) {
             editor.putBoolean("cancelHeartbeat", true)
