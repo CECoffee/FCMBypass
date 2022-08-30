db2a74ff2d2eacb6f600f843b23fd7e8862ed04b
moxi <moxiout@gmail.com>
2022-08-30 16:21:02 +0800

    fix: Switch status not stored

--- app/src/main/java/dev/cecoffee/antifcm/application/DefaultApplication.kt
+++ app/src/main/java/dev/cecoffee/antifcm/application/DefaultApplication.kt
@@ -1,9 +1,30 @@
 package dev.cecoffee.antifcm.application
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
 import com.highcapable.yukihookapi.hook.xposed.application.ModuleApplication
 
 class DefaultApplication : ModuleApplication() {
+    companion object {
+        lateinit var shared: SharedPreferences
+        fun isCancelHeartbeat(): Boolean {
+            return shared.getBoolean("cancelHeartbeat", true)
+        }
+
+        fun setCancelHeartbeatStatus(bool: Boolean): Boolean {
+            val result = shared.edit().putBoolean("cancelHeartbeat", bool)
+            return result.commit()
+        }
+
+        fun isBypassSdk(): Boolean {
+            return shared.getBoolean("bypassSdk", false)
+        }
+
+        fun setBypassSdkStatus(bool: Boolean): Boolean {
+            val result = shared.edit().putBoolean("bypassSdk", bool)
+            return result.commit()
+        }
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -13,5 +34,12 @@ class DefaultApplication : ModuleApplication() {
          */
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         // Your code here.
+        shared = this.applicationContext.getSharedPreferences("data", MODE_PRIVATE)
+        val editor = shared.edit()
+        if (!shared.contains("cancelHeartbeat")) {
+            editor.putBoolean("cancelHeartbeat", true)
+            editor.putBoolean("bypassSdk", false)
+            editor.apply()
+        }
     }
 }
